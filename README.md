a27fd110be611749698378a86b55d74220133a72
Kostiantyn Ochenash <Ochenashk@gmail.com>
2024-10-27 16:25:20 +0100

    feat: add eslint prettier

--- README.md
+++ README.md
@@ -1,36 +1,25 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
-
-## Getting Started
-
-First, run the development server:
-
 ```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+pwd
+cd D:/projects/next
+mkdir blog
+cd blog
+
+# init next
+ec next-app . # pnpm create next-app .
+code .
+mkdir -p content/posts
+code .prettierrc # https://github.com/tailwindlabs/prettier-plugin-tailwindcss
+
+# Install deps
+ea react-markdown @tailwindcss/typography @google-cloud/text-to-speech # pnpm add
+ec @eslint/config@latest
+ea -D @types/react @types/node @shadcn/ui prettier prettier-plugin-tailwindcss eslint-plugin-import @eslint/compat
+
+# Initialize shadcn/ui
+pnpm dlx shadcn@latest init
+pnpm dlx shadcn@latest add button card
+
+pnpm remove @tailwindcss/typography tailwind-merge tailwindcss-animate
+ea -D @tailwindcss/typography tailwind-merge tailwindcss-animate
+
 ```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
---
+++ eslint.config.mjs
@@ -0,0 +1,88 @@
+import { fixupPluginRules } from '@eslint/compat';
+import eJs from '@eslint/js';
+import pluginImport from 'eslint-plugin-import';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+import ts from 'typescript-eslint';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+const js = { configs: { all: eJs.configs.all, recommended: eJs.configs.recommended } };
+
+/** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigFile} */
+const eslintConfig = [
+  {
+    plugins: {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      import: fixupPluginRules(pluginImport),
+    },
+
+    settings: {
+      'import/parsers': {
+        espree: ['.js', '.cjs', '.mjs', '.jsx', '.mjs'],
+        '@typescript-eslint/parser': ['.ts', '.tsx', '.mjs'],
+      },
+      'import/extensions': ['.js', '.jsx', '.tsx', '.ts', '.mjs'],
+      'import/resolver': {
+        typescript: { alwaysTryTypes: true, project: './tsconfig.json' },
+        node: true,
+      },
+    },
+  },
+
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+
+        // tsconfigRootDir: '.',
+        project: ['./tsconfig.json'],
+        ecmaFeatures: { jsx: true },
+      },
+
+      globals: { ...globals.browser, ...globals.node },
+
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+
+      parser: ts.parser,
+    },
+  },
+  js.configs.recommended,
+  ...ts.configs.strictTypeChecked,
+  ...ts.configs.stylisticTypeChecked,
+  pluginReact.configs.flat.recommended,
+
+  {
+    // prettier-ignore
+
+    rules: {
+      'react/react-in-jsx-scope':'off',
+      '@typescript-eslint/consistent-type-definitions': ['warn', 'interface'],
+      '@typescript-eslint/consistent-type-assertions': ['error', { assertionStyle: 'never' }],
+      '@typescript-eslint/consistent-type-imports': ['warn',{ prefer: 'type-imports', fixStyle: 'separate-type-imports', }],
+      "@typescript-eslint/no-empty-interface": ["error", { allowSingleExtends: true }],
+      '@typescript-eslint/no-unused-vars': ['warn',{ argsIgnorePattern: '^_' }],
+      '@typescript-eslint/require-await': 'warn',
+      '@typescript-eslint/no-misused-promises': ['warn',{ checksVoidReturn: { attributes: false } }],
+      "@typescript-eslint/no-confusing-void-expression": ["error", { ignoreArrowShorthand: true, ignoreVoidOperator: true }],
+
+
+      // personal
+      'linebreak-style': ['error', 'unix'],
+      'no-shadow': 'error',
+      'no-restricted-imports': 'warn',
+      'no-param-reassign': ['error', { props: true, /* ignorePropertyModificationsFor: ['req'] */  }],
+      'object-shorthand': 'warn',
+      'sort-imports': ['error', { ignoreCase: true, ignoreDeclarationSort: true }],
+      'import/no-unresolved': 'error',
+      'import/newline-after-import': 'error',
+      'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+      'import/order': [ 'error', { 'newlines-between': 'always', warnOnUnassignedImports: true, groups: [ 'builtin', 'external', 'internal', ['parent', 'sibling', 'index'], 'type', 'unknown', ], alphabetize: { order: 'asc', caseInsensitive: true, }, },
+      ],
+    },
+  },
+];
+
+export default eslintConfig;
